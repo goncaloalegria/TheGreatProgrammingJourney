025e397db55175a576173e6e75c342df277fe5b6
goncalo-alegria-a22408663 <a22408663@alunos.ulht.pt>
2025-10-29 09:11:22 +0000

    criação de classe e funções

---
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -0,0 +1,57 @@
+package pt.ulusofona.lp2.greatprogrammingjourney;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GameManager {
+
+    String ID;
+
+    public boolean createInitialBoard(String[][] playerInfo, int worldSize) {
+
+    }
+
+    public String getImagePng(int nrSquare) {
+
+    }
+
+    public String[] getProgrammerInfo(int id) {
+        // Implementation goes here
+    }
+
+    public String getProgrammerInfoAsStr(int id) {
+
+    }
+
+    public String[] getSlotInfo(int position) {
+
+    }
+
+    public int getCurrentPlayerID() {
+
+    }
+
+    public boolean moveCurrentPlayer(int nrSpaces) {
+
+    }
+
+    public boolean gameIsOver() {
+
+    }
+
+    public ArrayList<String> getGameResults() {
+
+    }
+
+    public JPanel getAuthorsPanel() {
+
+    }
+
+    public HashMap<String, String> customizeBoard() {
+
+    }
+
+
+
+}
